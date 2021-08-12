ab9e37b24db4f44fbd5ee295c6803ef354e76e9e
Hoc Le <hvle@openwt.com>
2021-08-12 23:28:32 +0700

    add current user

--- lib/canvas_ems.rb
+++ lib/canvas_ems.rb
@@ -3,4 +3,9 @@ require "canvas_ems/version"
 module CanvasEms
   class Error < StandardError; end
   # Your code goes here...
+  class User
+    def current_user
+      render json: @current_user
+    end
+  end
 end

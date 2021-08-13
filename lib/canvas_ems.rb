4ad4cb8b8b4cdeb7aab1b2cb73e4d0b791521c83
Hoc Le <hvle@openwt.com>
2021-08-13 10:58:57 +0700

    Update current user

--- lib/canvas_ems.rb
+++ lib/canvas_ems.rb
@@ -2,10 +2,11 @@ require "canvas_ems/version"
 
 module CanvasEms
   class Error < StandardError; end
-  # Your code goes here...
   class UserGem
+
     def current_user
-      User.last
+      @current_user
     end
+    
   end
 end

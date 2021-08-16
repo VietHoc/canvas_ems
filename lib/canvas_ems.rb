c7a3dcc043b0fc2d31d68fed286def7b07c7fb8c
Hoc Le <hvle@openwt.com>
2021-08-16 22:51:33 +0700

    add ios

--- lib/canvas_ems.rb
+++ lib/canvas_ems.rb
@@ -8,6 +8,14 @@ module CanvasEms
       User.last
     end
 
+    def ios_devices
+      Api::V1::ReceiptsController.new.ios_devices?
+    end
+
+    def ios
+      Api::V1::ReceiptsController.new.ios
+    end
+
     def current_user
       Api::V1::ReceiptsController.new.current_user
     end
